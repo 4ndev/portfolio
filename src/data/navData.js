b53e4b47b3d03a9e21dc488813cd4a8b119acbab
4ndev <4ndrei.93@gmail.com>
2023-07-29 12:23:41 +0200

    Añadido nuevo componentes y sección de experiencias.

---
+++ src/data/navData.js
@@ -0,0 +1,14 @@
+export const socialData = [
+  {
+    name: 'GitHub',
+    url: 'https://github.com/4ndev',
+  },
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/4ndev_',
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/andres-alvarado-dev/',
+  }
+]
