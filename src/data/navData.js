7699acd6d9ff83e419fd6a37830d95a587ad94f9
4ndev <4ndrei.93@gmail.com>
2023-07-31 16:27:58 +0200

    Update social media

--- src/data/navData.js
+++ src/data/navData.js
@@ -2,13 +2,16 @@ export const socialData = [
   {
     name: 'GitHub',
     url: 'https://github.com/4ndev',
+    icon: 'mdi:github',
   },
   {
     name: 'Twitter',
     url: 'https://twitter.com/4ndev_',
+    icon: 'mdi:twitter',
   },
   {
     name: 'LinkedIn',
     url: 'https://www.linkedin.com/in/andres-alvarado-dev/',
+    icon: 'mdi:linkedin',
   }
 ]
